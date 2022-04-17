9d4c3026ec88ab33d5a5afb43d0ef13370676519
Cerus <m.dorn.2003@web.de>
2022-04-17 04:15:40 +0200

    Initial commit

---
+++ settings/authors.js
@@ -0,0 +1,22 @@
+// create the authors for the blog
+// const authors = {
+//     "author-id": {
+//         name: "Author Name",
+//         description: "Some information about the author", **you can use markup!**", 
+//         metaDescription: "This is optional, but if you used markup in your description, include a plain text description here for SEO",
+//     },
+// }
+// if the blog doesn't need authors, empty the authors object e.g const authors = {};
+
+const authors = {
+    "system": {
+        name: "System",
+        description: "System"
+    },
+    "cerus": {
+        name: "CMDR Cerus",
+        description: "Full time space explorer. Always on the hunt for new discoveries."
+    }
+};
+
+export default authors;
